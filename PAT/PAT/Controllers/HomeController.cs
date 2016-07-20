75a61f53eee7aafc56d7e4b7f86fa7b4e888e099
Truong Xuan <truongxuan92vt@gmail.com>
2016-07-20 13:45:18 +0700

    1

--- PAT/PAT/Controllers/HomeController.cs
+++ PAT/PAT/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using ManageShop.Models;
 namespace ManageShop.Controllers
 {
     public class HomeController : Controller
@@ -15,7 +15,24 @@ namespace ManageShop.Controllers
 
         public ActionResult Admin()
         {
-            return View();
+            List<TreeViewModel> list = new List<TreeViewModel>();
+            PATDBDataContext db = new PATDBDataContext();
+            var ls_category = db.Categories.Where(x => x.Status == "A" && x.ParentID == 0 && x.Reftype=="C").ToList();
+            foreach(var item in ls_category){
+                TreeViewModel category = new TreeViewModel();
+                category.ID = item.ID;
+                category.Name = item.CategoryName;
+                var ls_category_child = db.Categories.Where(x => x.Status == "A" && x.ParentID == item.ID && x.Reftype == "C").ToList();
+                foreach (var child in ls_category_child)
+                {
+                    TreeViewModel ls_child = new TreeViewModel();
+                    ls_child.ID = child.ID;
+                    ls_child.Name = child.CategoryName;
+                    category.Child.Add(ls_child);
+                }
+                list.Add(category);
+            }
+            return View(list);
         }
 
         public ActionResult About()
