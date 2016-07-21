6e05aa9055e929080f54e2a7204158a8bd341d56
babystarlovely <phamhongtuanvn@gmail.com>
2016-07-21 08:36:00 +0700

    u

---
+++ PAT/PAT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ManageShop.Startup))]
+namespace ManageShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
