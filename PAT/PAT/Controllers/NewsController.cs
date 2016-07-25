70b9e556fc2d436f64d3018cb60c2632549300ca
truongxuan92vt <truongxuan92vt@gmail.com>
2016-07-25 23:56:57 +0700

    create NewsController.cs

---
+++ PAT/PAT/Controllers/NewsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ManageShop.Models;
+
+namespace ManageShop.Controllers
+{
+    public class NewsController:Base
+    {
+        public ActionResult Index()
+        {
+            List<TreeViewModel> list = new List<TreeViewModel>();
+            PATDBDataContext db = new PATDBDataContext();
+            var ls_category = db.Categories.Where(x => x.Status == "A" && x.ParentID == 0 && x.Reftype == "C").ToList();
+            foreach (var item in ls_category)
+            {
+                TreeViewModel category = new TreeViewModel();
+                category.ID = item.ID;
+                category.Name = item.CategoryName;
+                var ls_category_child = db.Categories.Where(x => x.Status == "A" && x.ParentID == item.ID && x.Reftype == "C").ToList();
+                foreach (var child in ls_category_child)
+                {
+                    TreeViewModel ls_child = new TreeViewModel();
+                    ls_child.ID = child.ID;
+                    ls_child.Name = child.CategoryName;
+                    category.Child.Add(ls_child);
+                }
+                list.Add(category);
+            }
+            return View(list);
+        }
+        public ActionResult CreateNews(NewsModel data)
+        {
+            PATDBDataContext db = new PATDBDataContext();
+            NewsModel model = new NewsModel();
+            return View(model);
+        }
+    }
+}
